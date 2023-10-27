317c01a8ef69c0a085bb34a0cd98cab4fe01b17b
mondadori89 <mondadori@DESKTOP-F3SADT1>
2023-10-27 12:04:00 -0300

    App fully functional

--- app.py
+++ app.py
@@ -63,15 +63,11 @@ def upload():
 
     # Generate a unique folder name based on the email using UUID
     inputs_folder_path = os.path.join(app.config['IMAGES_FOLDER'], email, conversion_id, app.config['INPUTS_FOLDER'])
-    
-    # Create the folder if it doesn't exist
-    os.makedirs(inputs_folder_path, exist_ok=True)
 
     # Save each image file to the folder
-    print(f'Uploading: Email: {email} - ProjectID: {conversion_id} - Image: {image_file}')
+    print(f'Uploading file to S3: Email: {email} - ProjectID: {conversion_id} - Image: {image_file}')
     if image_file and allowed_file(image_file.filename):
         filename = secure_filename(image_file.filename)
-        # time.sleep(2)
         s3.upload_fileobj(image_file, s3_bucket, os.path.join(inputs_folder_path, filename))
     else:
         return "Invalid file format.", 400
@@ -102,6 +98,7 @@ def create_checkout_session():
                 'quantity': 1,
             }],
             customer_email = email,
+            metadata = { "conversion_id": conversion_id },
             mode = 'payment',
             success_url = 'http://localhost:5173/convert?session_id={CHECKOUT_SESSION_ID}',  # Replace with your success URL
             cancel_url = 'http://localhost:5173/',  # Replace with your cancel URL
@@ -118,22 +115,57 @@ def create_checkout_session():
         return {'error': str(e)}
 
 
+@app.route('/check_stripe_session_info', methods=['POST'])
+def check_stripe_session_info():
+    session_id = request.form['session_id']   
+
+    try:
+        stripe_session = stripe.checkout.Session.retrieve(session_id)
+
+        response_data = {
+            "msg": f"session info fetched.",
+            "stripe_session": stripe_session
+        }
+
+        return response_data, 200  
+    
+    except Exception as e:
+        return {'error': str(e)}
+
+
 @app.route('/convert', methods=['POST'])
 def convert():
     email = request.form['email']
     conversion_id = request.form['conversion_id']
-    session_id = request.form['session_id']  # Get the session ID from the request
+    # session_id = request.form['session_id']  # Get the session ID from the request
 
-    session = stripe.checkout.Session.retrieve(session_id)
+    # session = stripe.checkout.Session.retrieve(session_id)
 
-    if session.payment_status != 'paid':
-        return {'error': 'Payment not successful.'}, 400
+    # if session.payment_status != 'paid':
+    #     return {'error': 'Payment not successful.'}, 400
 
     inputs_folder_path = os.path.join(app.config['IMAGES_FOLDER'], email, conversion_id, app.config['INPUTS_FOLDER'])
     output_folder_path = os.path.join(app.config['IMAGES_FOLDER'], email, conversion_id, app.config['OUTPUTS_FOLDER'])
     
-    # Create the folder if it doesn't exist
+    # Create the folders if it doesn't exist
+    os.makedirs(inputs_folder_path, exist_ok=True)
     os.makedirs(output_folder_path, exist_ok=True)
+
+    # Download input images from S3
+    input_files_list = s3.list_objects(Bucket=s3_bucket, Prefix=inputs_folder_path)
+
+    if 'Contents' not in input_files_list:
+        return "No files found for the provided email and conversion_id.", 404
+    
+    for obj in input_files_list['Contents']:
+        file_name = obj['Key'].split('/')[-1]
+        file_path = os.path.join(inputs_folder_path, file_name)
+        
+        # Skip if directory (S3 'folders' or empty files are listed as keys with size 0)
+        if obj['Size'] == 0:
+            continue
+        
+        s3.download_file(s3_bucket, obj['Key'], file_path)
 
     # Convert images from the input_folder to the output_folder
     convert_jpg_tiff(inputs_folder_path, output_folder_path)
@@ -147,15 +179,25 @@ def convert():
                 zipf.write(file_path, os.path.relpath(file_path, output_folder_path))
         print(f'Files zipped on path: {zip_file_path}')
 
+    # Upload zipfile to S3
+    print(f'\n Uploading Zip file to S3: Email: {email} - ProjectID: {conversion_id} - Zipfile: {zip_file_path}')
+    zip_s3_path = os.path.join(zip_file_path)
+    s3.upload_file(zip_file_path, s3_bucket, zip_s3_path)  
+    
+    zipfile_url = f"https://{s3_bucket}.s3.amazonaws.com/{zip_s3_path}"
+    print(f'\n Zip file S3 download url: {zipfile_url}')
+    
     # Send the zip file via email
     with app.app_context():
         msg = Message(f'Image Conversion Results ID: {conversion_id}', sender='thermoconv@gmail.com', recipients=[email])
-        msg.body = 'Please find the converted images attached.'
-        with app.open_resource(zip_file_path) as attachment:
-            msg.attach(conversion_id + '_outputs.zip', 'application/zip', attachment.read())
+        msg.body = f'Please find the converted images in the link below: \n Download Link: {zipfile_url}'
         print(f'Files on ID: {conversion_id} about to be send to {email}')
         mail.send(msg)
         print('Files sent')
+
+    # Delete input and outputs from server
+    folder_to_delete_path = os.path.join(app.config['IMAGES_FOLDER'], email, conversion_id)
+    shutil.rmtree(folder_to_delete_path)
 
     # Data to return
     response_data = {
